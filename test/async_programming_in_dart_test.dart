a7c443d3ec4ce189bd922d3db7784cacc7fc373a
chiagoziemihejirika <ihejigozie3@gmail.com>
2023-10-21 18:28:17 +0200

    Initial commit

---
+++ test/async_programming_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:async_programming_in_dart/async_programming_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
